63f1df38edeef8e005a4d960cc26b37dcb1c3d9b
514840279 <514840279@qq.com>
2019-10-31 17:14:00 +0800

    first commit

---
+++ src/main/java/org/danyuan/application/DatatransformApplication.java
@@ -0,0 +1,15 @@
+package org.danyuan.application;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class DatatransformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatatransformApplication.class, args);
+	}
+
+}
