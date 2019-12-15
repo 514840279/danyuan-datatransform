5a1f6e9900a647359e63f515546414806299e98a
王滕辉 <514840279@qq.com>
2019-12-15 12:15:49 +0800

    1

--- src/main/java/org/danyuan/application/imp/Testaccess.java
+++ src/main/java/org/danyuan/application/imp/Testaccess.java
@@ -28,22 +28,22 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class Testaccess {
-
+	
 	// 负责创建任务
 	@Autowired
 	private JobBuilderFactory	jobBuilderFactory;
-	
+
 	// 负责创建step 任务由step决定
 	@Autowired
 	private StepBuilderFactory	stepBuilderFactory;
-	
+
 	@Autowired
-	private MyItemWriterDemo2	myItemWriterDemo2;
-	
+	private ItemWriterToOracle	itemWriterToOracle;
+
 	// 创建job 数据库读取
 	@Bean
 	public Job jdbcItemReadJobDemo() {
-		
+
 		return jobBuilderFactory
 		        // 名称是job的标识符，
 		        .get("jdbcItemReadJobDemo")
@@ -54,7 +54,7 @@ public class Testaccess {
 		        //
 		        .build();
 	}
-
+	
 	/**
 	 * @方法名 itemReadstepDemo
 	 * @功能 TODO(这里用一句话描述这个方法的作用)
@@ -67,11 +67,11 @@ public class Testaccess {
 	public Step jdbcitemReadstepDemo() {
 		return stepBuilderFactory.get("jdbcitemReadstepDemo")
 		        // 创建 tasklet 或者chunk（itemread，itemprocess，itemwrite）
-		        .<Map<String, Object>, Map<String, Object>> chunk(1000)
+		        .<Map<String, Object>, Map<String, Object>> chunk(10000)
 		        // 读取对象
-		        .reader(jdbcitemReaderDemo())
+		        .reader(accessitemReader())
 		        // 测试输出
-		        .writer(myItemWriterDemo2)
+		        .writer(itemWriterToOracle)
 				// 自定义输出到文件
 //		        .writer(myFlatFileItemWriter)
 				// 自定义输出到xml中
@@ -81,25 +81,17 @@ public class Testaccess {
 		        //
 		        .build();
 	}
-
-	/**
-	 * @方法名 itemReaderDemo
-	 * @功能 TODO(这里用一句话描述这个方法的作用)
-	 * @参数 @return
-	 * @返回 ItemReader<? extends String>
-	 * @author Administrator
-	 * @throws
-	 */
+	
 	@Bean
 	@StepScope
-	public JdbcPagingItemReader<Map<String, Object>> jdbcitemReaderDemo() {
+	public JdbcPagingItemReader<Map<String, Object>> sqlserveritemReader() {
 		JdbcPagingItemReader<Map<String, Object>> reader = new JdbcPagingItemReader<>();
-		DataSource dataSource = DataSourceBuilder.create().driverClassName("net.ucanaccess.jdbc.UcanaccessDriver").url("jdbc:ucanaccess://" + "F:\\2000年企业数据库.mdb").build();
+		DataSource dataSource = DataSourceBuilder.create().driverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver").url("jdbc:sqlserver://127.0.0.1:1433;DatabaseName=51part2_hr_6").password(".").username("sa").build();
 		// 设置数据源
 		reader.setDataSource(dataSource);
 		// 每次读取记录数
-		reader.setFetchSize(100);
-		
+		reader.setFetchSize(1000);
+
 		// 结果映射对象
 		reader.setRowMapper((rs, rownum) -> {
 			ResultSetMetaData metaData = rs.getMetaData();
@@ -112,20 +104,60 @@ public class Testaccess {
 			return map;
 		});
 		// 指定查询语句
-
+		
 //		MySqlPagingQueryProvider pagingQueryProvider = new MySqlPagingQueryProvider();
 		SqlServerPagingQueryProvider pagingQueryProvider = new SqlServerPagingQueryProvider();
 //		OraclePagingQueryProvider pagingQueryProvider = new OraclePagingQueryProvider();
 		pagingQueryProvider.setSelectClause("*");
-		pagingQueryProvider.setFromClause(" From `2000` ");
+		pagingQueryProvider.setFromClause(" From workexp ");
 		// 排序
 		Map<String, Order> sort = new HashMap<>();
-		sort.put("法人代码", Order.ASCENDING);
+		sort.put("userid", Order.ASCENDING);
 		pagingQueryProvider.setSortKeys(sort);
-
+		
 		//
 		reader.setQueryProvider(pagingQueryProvider);
-
+		
 		return reader;
 	}
+	
+	@Bean
+	@StepScope
+	public JdbcPagingItemReader<Map<String, Object>> accessitemReader() {
+		JdbcPagingItemReader<Map<String, Object>> reader = new JdbcPagingItemReader<>();
+		DataSource dataSource = DataSourceBuilder.create().driverClassName("net.ucanaccess.jdbc.UcanaccessDriver").url("jdbc:ucanaccess://" + "F:\\data\\企业名录\\2008年中国工业企业数据库.mdb").build();
+		// 设置数据源
+		reader.setDataSource(dataSource);
+		// 每次读取记录数
+		reader.setFetchSize(1000);
+
+		// 结果映射对象
+		reader.setRowMapper((rs, rownum) -> {
+			ResultSetMetaData metaData = rs.getMetaData();
+			int count = metaData.getColumnCount();
+			Map<String, Object> map = new HashMap<>();
+			for (int i = 1; i <= count; i++) {
+				String columnName = metaData.getColumnName(i);
+				map.put(columnName, rs.getObject(columnName));
+			}
+			return map;
+		});
+		// 指定查询语句
+		
+//		MySqlPagingQueryProvider pagingQueryProvider = new MySqlPagingQueryProvider();
+		SqlServerPagingQueryProvider pagingQueryProvider = new SqlServerPagingQueryProvider();
+//		OraclePagingQueryProvider pagingQueryProvider = new OraclePagingQueryProvider();
+		pagingQueryProvider.setSelectClause("*");
+		pagingQueryProvider.setFromClause(" From `qycw2008` ");
+		// 排序
+		Map<String, Order> sort = new HashMap<>();
+		sort.put("法人单位", Order.ASCENDING);
+		pagingQueryProvider.setSortKeys(sort);
+		
+		//
+		reader.setQueryProvider(pagingQueryProvider);
+		
+		return reader;
+	}
+	
 }
